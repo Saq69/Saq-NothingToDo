ea5871ca90eb6d9215e4da1b713f56ada4b12556
Saq <74090118+Saq69@users.noreply.github.com>
2024-12-01 09:55:43 -0800

    Added An arabic Config

--- client.lua
+++ client.lua
@@ -1,16 +1,3 @@
-local disabledControls = {
-    [21] = true, -- Sprinting
-    [22] = true, -- Jumping
-    [37] = true, -- Weapon wheel
-    [45] = true, -- Reloading
-    [24] = true, -- Punching
-    [69] = true, -- Melee attack (primary)
-    [92] = true, -- Melee attack (secondary)
-    [106] = true, -- Hitting with weapon
-    [140] = true, -- Reloading and melee attacks
-    [141] = true  -- Stabbing with knife
-}
-
 RegisterNetEvent('Saq:StartNothingToDo')
 AddEventHandler('Saq:StartNothingToDo', function(time)
     StartNothingToDo(time)
@@ -20,34 +7,31 @@ function StartNothingToDo(time)
     local isInRestrictedState = true
     local playerPed = PlayerPedId()
     local vehicle = GetVehiclePedIsIn(playerPed, false)
-    if time == nil then time = 30 end
+    if time == nil then time = Config["الوقت الافتراضي"] end
 
     local vehicles = {}
-
     local initialSpeed = GetVehicleHandlingFloat(vehicle, "CHandlingData", "fInitialDriveMaxFlatVel")
-
     vehicles[vehicle] = initialSpeed
 
-
     Citizen.CreateThread(function()
-        TriggerEvent('pogressBar:drawBar', time*1000, '<font size=5 color=orange>قدراتك محدوده حتى انتهاء الوقت')
-        Wait(time*1000) 
-        isInRestrictedState = false 
+        TriggerEvent('pogressBar:drawBar', time * 1000, '<font size=5 color=orange>قدراتك محدوده حتى انتهاء الوقت')
+        Wait(time * 1000)
+        isInRestrictedState = false
         for k, v in pairs(vehicles) do
             SetEntityMaxSpeed(k, v)
-            table.remove(vehicles, k) 
+            table.remove(vehicles, k)
         end
     end)
 
     Citizen.CreateThread(function()
         while isInRestrictedState do
-            Wait(5) 
+            Wait(5)
 
-            SetEntityMaxSpeed(vehicle, 9.0)
-            SetCurrentPedWeapon(playerPed, GetHashKey("WEAPON_UNARMED"), true) -- Remove weapon from hand
-            DisablePlayerFiring(playerPed, true) -- Disable shooting
+            SetEntityMaxSpeed(vehicle, Config["سرعة المركبة"])
+            SetCurrentPedWeapon(playerPed, GetHashKey("WEAPON_UNARMED"), true) -- إزالة السلاح من اليد
+            DisablePlayerFiring(playerPed, true) -- تعطيل إطلاق النار
 
-            for control, _ in pairs(disabledControls) do
+            for control, _ in pairs(Config["الأزرار الممنوعة"]) do
                 DisableControlAction(0, control, true)
             end
         end
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,6 @@
 fx_version 'adamant'
-
 game 'gta5'
-
 description 'Saq NothingToDo'
 
+client_script 'config.lua' 
 client_script 'client.lua'
